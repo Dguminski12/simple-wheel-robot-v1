027da19b4ad2f3e5f75d7d5dc83cf263e5957e4e
Dguminski12 <davidguminski12@gmail.com>
2026-02-05 14:17:41 +0000

    defined left motors and tested foward movement

---
+++ src/main.py
@@ -0,0 +1,22 @@
+from machine import Pin, PWM
+import time
+
+# GPIO Pins 
+AIN1 = Pin(25, Pin.OUT)
+AIN2 = Pin(26, Pin.OUT)
+PWMA = PWM(Pin(27), freq=1000)
+
+PWMA.duty(0) # Start with motor stopped
+
+while True:
+
+    #Forwards
+    AIN1.on()
+    AIN2.off()
+    PWMA.duty(512) # Set motor speed to 50%
+    time.sleep(2) # Run forward for 2 seconds
+
+    PWMA.duty(0) # Stop motor
+    time.sleep(1) # Pause for 1 second
+
+    
